940b49e88b61b95792c5a6f1be0c18a7ea7b6682
Tunafan <124045808+Tunafan@users.noreply.github.com>
2023-04-27 13:20:22 +0200

    add console.log

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ window.addEventListener("load", initApp);
 
 function initApp() {
   console.log("Pull op, pull ooooooop");
+  rendMig();
+}
+
+function rendMig() {
+  console.log("mellem ballerne");
 }
